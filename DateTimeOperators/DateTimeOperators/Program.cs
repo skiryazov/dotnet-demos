b3ecdedf3106507a9a79e416473930a4f375bce4
Stefan Kiryazov <stefan.kiryazov@gmail.com>
2019-08-08 21:25:39 +0200

    Adding DateTimeOperators

---
+++ DateTimeOperators/DateTimeOperators/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.UtcNow;
+            DateTime yesterday = now.AddDays(-1);
+
+            // Can you guess the datatype of the result? Point at the "var" to reveal it
+            var what = now - yesterday;
+            Console.WriteLine(what);
+
+            TimeSpan oneDay = new TimeSpan(1, 0, 0, 0);
+            // What about this one?
+            var what2 = now + oneDay;
+            Console.WriteLine(what2);
+
+            // And this?
+            var what3 = oneDay + oneDay;
+            Console.WriteLine(what3);
+
+            RationalNumber x = new RationalNumber(1, 2);
+            // Go to the definition of RationalNumber to see how we implemented the custom multiplication
+            Console.WriteLine(x*x);
+        }
+    }
+}
---
+++ DateTimeOperators/DateTimeOperators/RationalNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeOperators
+{
+    struct RationalNumber
+    {
+        int p;
+        int q;
+
+        public RationalNumber(int p, int q)
+        {
+            this.p = p;
+            this.q = q;
+        }
+
+        public static RationalNumber operator*(RationalNumber one, RationalNumber other)
+        {
+            return new RationalNumber(one.p * other.p, other.q * one.q);
+        }
+
+        public override string ToString()
+        {
+            return p + "\\" + q;
+        }
+    }
+}
