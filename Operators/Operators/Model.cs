4aadc8b4a159454652b5365f7b9eedff51919638
Stefan Kiryazov1 <stefan.kiryazov@gmail.com>
2020-02-13 19:35:45 +0100

    Added operators

---
+++ Operators/Operators/Model.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class User
+    {
+        public Address Address { get; set; }
+    }
+    class Employee : User
+    {
+        public DateTime DateOfBirth { get; set; }
+        public String JobTitle { get; set; }
+        public Sale[] Sales { get; set; }
+    }
+
+    class Address
+    {
+        public String City { get; set; }
+        public String Street { get; set; }
+    }
+
+    class Sale
+    {
+        public User Customer { get; set; }
+        public Decimal Amount { get; set; }
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+
+
+            // the oldschool version
+            if (employee!=null && employee.Sales!=null && employee.Sales.Length>0 && 
+                employee.Sales[0]!=null && employee.Sales[0].Customer!=null &&
+                employee.Sales[0].Customer.Address!= null )
+            {
+                String city = employee.Sales[0].Customer.Address.City;
+            }
+
+            // the Elvis version
+            String eCity = employee?.Sales?[0]?.Customer?.Address?.City;
+
+            // nullable Decimal
+            Decimal? amount = employee?.Sales?[0]?.Amount;
+
+            String formattedCity;
+            // traditional version
+            if (eCity == null)
+            {
+                formattedCity = "unknown";
+            }
+            else
+            {
+                formattedCity = eCity;
+            }
+
+            // ternary operator version
+            formattedCity = eCity == null ? "unknown" : eCity;
+            // null-coalescing operator version
+            formattedCity = eCity ?? "unknown";
+            // null-coalescing assignment
+            //eCity ??= "unknown"; // works only in C# 8.0 and above
+        }
+    }
+}
