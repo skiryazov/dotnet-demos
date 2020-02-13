fb8f15be1c53ffbb5fcfee3327097e38ccf0922e
Stefan Kiryazov1 <stefan.kiryazov@gmail.com>
2020-02-13 15:05:50 +0100

    Added demo for writing to windows log

---
+++ WriteToWindowsLogs/WriteToWindowsLogs/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WriteToWindowsLogs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteToApplicationLog();
+            WriteToCustomSource();
+        }
+
+        static void WriteToApplicationLog()
+        {
+            using (EventLog eventLog = new EventLog("Application"))
+            {
+                eventLog.Source = "Application";
+                eventLog.WriteEntry("Works!!!", EventLogEntryType.Warning, 101, 1);
+            }
+        }
+
+        static void WriteToCustomSource()
+        {
+            string source = "WriteToLogDemo";
+            using (EventLog applicationLog = new EventLog("Application"))
+            { 
+                if (!EventLog.SourceExists(source))
+                {
+                    EventLog.CreateEventSource(source, "Application");
+                }
+                applicationLog.Source = source;
+                applicationLog.WriteEntry("Works with custom source", 
+                    EventLogEntryType.Warning, 101, 1);
+            }
+        }
+    }
+}
